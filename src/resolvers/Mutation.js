a34d3d5767025c1bc53711e2888c1bf262116b76
Tri Hoang <trihoang@bu.edu>
2019-06-06 00:08:17 -0400

    Query and Mutation done

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,47 @@
+import { ApolloError, UserInputError } from 'apollo-server';
+import { Room, Message } from '../mongos';
 
 const Mutation = {
+	addRoom: async (_, args) => {
+		try {
+			/* Check if room already exists, stop if it does */
+			const checkRoom = await Room.findOne({ roomName: args.roomName });
+			if (checkRoom) {
+				throw new UserInputError("Room already exists");
+			}
+
+			/* Create the room and return it */
+			const newRoom = new Room({ ...args });
+			return await newRoom.save();
+
+		} catch (E) {
+			throw new ApolloError("can't addRoom", E);
+		}
+	},
+
+	addMessage: async (_, args) => {
+		try {
+			/* Find the room, stop if cannot find it */
+			const findRoom = await Room.findOne({ roomName: args.roomName });
+			if (!findRoom) {
+				throw new UserInputError("Can't find the room");
+			}
+
+			/* Create the new Message object */
+			const newMessage = new Message({ ...args, roomRef: findRoom });
+			await newMessage.save();
+
+			/* Update room's messages list with the new Message ref */
+			findRoom.messages.push(newMessage);
+			await findRoom.save();
+
+			return newMessage;
+
+		} catch (E) {
+			throw new ApolloError("can't addMessage", E);
+		}
+	},
+
 
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,22 @@
+import { ApolloError, UserInputError } from "apollo-server";
+import { Room, Message } from '../mongos';
 
 const Query = {
-
+	getMessages: async (_, args) => {
+		try {
+			/* Find the room, stop if cannot find it */
+			const findRoom = await Room.findOne({ roomName: args.roomName });
+			if (!findRoom) {
+				throw new UserInputError("Can't find the room");
+			}
+			
+			/* Populate the messages field and return it */
+			await findRoom.populate("messages").execPopulate();
+			return findRoom.messages;
+		} catch (E) {
+			throw new ApolloError("can't getMessages", E);
+		}
+	}
 };
 
 export default Query;
